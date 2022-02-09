141c8a5610b2761dc3128fe2af8d364aa689fd97
Luciana Bischoff <lu_bischoff@hotmail.com>
2022-02-09 18:26:07 -0300

    [Grupo 2] Project Front-End Online Store - Requisito 4

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,9 +2,29 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 // import PropTypes from 'prop-types';
 // import Cart from './Cart';
+import { getCategories } from '../services/api';
 
 class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      products: [],
+    };
+    this.handleRequest = this.handleRequest.bind(this);
+  }
+
+  componentDidMount() {
+    this.handleRequest();
+  }
+
+  async handleRequest() {
+    const response = await getCategories();
+    this.setState({ products: response });
+  }
+
   render() {
+    const { products } = this.state;
+
     return (
       <div>
         <form>
@@ -24,6 +44,18 @@ class Home extends Component {
             </Link>
           </div>
         </form>
+        <fieldset>
+          { products.map(({ id, name }) => (
+            <label htmlFor={ name } data-testid="category" key={ id }>
+              <input
+                type="radio"
+                value={ name }
+                name="name"
+              />
+              { name }
+            </label>
+          )) }
+        </fieldset>
       </div>
     );
   }
