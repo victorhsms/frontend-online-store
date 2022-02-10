58e8e91202d5a8833a9dd1ec728e6339188eea9e
Victor Hugo <victorhsmsantos@gmail.com>
2022-02-10 14:59:27 -0300

    [Grupo 2] Atualizaçao do api.js - Requisito 1

--- src/services/api.js
+++ src/services/api.js
@@ -6,13 +6,35 @@ export async function getCategories() {
   return response;
 }
 
+export async function getProductsFromCategoryId(categoryId) {
+  const fetchUrl = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`;
+
+  const requestAPi = await fetch(fetchUrl);
+  const response = await requestAPi.json();
+
+  return response;
+}
+
+export async function getProductsFromQuery(query) {
+  const fetchUrl = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
+
+  const requestAPi = await fetch(fetchUrl);
+  const response = await requestAPi.json();
+
+  return response;
+}
+
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
-  let fetchUrl;
-  if (!query) {
-    fetchUrl = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`;
-  } else {
-    fetchUrl = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
-  }
+  const fetchUrl = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
+
+  const requestAPi = await fetch(fetchUrl);
+  const response = await requestAPi.json();
+
+  return response;
+}
+
+export async function getProductsFromProductId(productId) {
+  const fetchUrl = `https://api.mercadolibre.com/items/${productId}`;
 
   const requestAPi = await fetch(fetchUrl);
   const response = await requestAPi.json();
